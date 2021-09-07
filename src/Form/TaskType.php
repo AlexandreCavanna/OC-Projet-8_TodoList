f49f2625b821406ccb961fb48b8a88b3d9d48843
AlexandreCavanna <alexandre.cavanna.pro@gmail.com>
2021-09-08 00:55:10 +0200

    task has to be attached to one user

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -5,15 +5,36 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Security\Core\Security;
 
 class TaskType extends AbstractType
 {
+    private Security $user;
+
+    /**
+     * TaskType constructor.
+     * @param Security $user
+     */
+    public function __construct(Security $user)
+    {
+        $this->user = $user;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
             ->add('content', TextareaType::class)
-            //->add('author') ===> must be the user authenticated
         ;
+
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+            $user = $this->user->getUser();
+
+            if ($user) {
+                $event->getData()->setAuthor($user);
+            }
+        });
     }
 }
